b2369150f89ce6b020371f6ff11f999a925a7df7
skavt <saliikavt@gmail.com>
2021-01-21 00:51:49 +0400

    Implement bottom navigation functionality

--- app/src/main/java/com/example/e_books/activities/MainActivity.kt
+++ app/src/main/java/com/example/e_books/activities/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.e_books.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.e_books.R
+import com.example.e_books.fragments.CategoryFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +18,35 @@ class MainActivity : AppCompatActivity() {
         findNavController(R.id.fragment).addOnDestinationChangedListener { _, destination, _ ->
             supportActionBar?.setDisplayHomeAsUpEnabled(R.id.category_fragment != destination.id)
         }
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setOnNavigationItemSelectedListener(navListener)
+
+        when (savedInstanceState) {
+            null -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment, CategoryFragment())
+                .commit()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    private val navListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var selectedFragment: Fragment? = null
+
+        when (item.itemId) {
+            R.id.nav_home -> selectedFragment = CategoryFragment()
+            // TODO add nav fragments
+        }
+
+        when {
+            selectedFragment != null -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment, selectedFragment)
+                .commit()
+        }
+        true
     }
 }
