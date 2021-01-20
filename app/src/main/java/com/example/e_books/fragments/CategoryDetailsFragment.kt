552a4015e8bf215a6df22682798c8ab81c4105f0
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2021-01-20 12:50:37 +0400

    Implement category details page

--- app/src/main/java/com/example/e_books/fragments/CategoryDetailsFragment.kt
+++ app/src/main/java/com/example/e_books/fragments/CategoryDetailsFragment.kt
@@ -1,7 +1,42 @@
 package com.example.e_books.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.navGraphViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.e_books.R
+import com.example.e_books.adapters.CategoryDetailsAdapter
+import com.example.e_books.model.Books
+import com.example.e_books.services.BookLiveData
+import kotlinx.android.synthetic.main.category_details_fragment.*
 
 class CategoryDetailsFragment : Fragment(R.layout.category_details_fragment) {
+
+    private lateinit var categoryDetailsView: View
+    private val bookLiveData: BookLiveData by navGraphViewModels(R.id.books_nav)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        categoryDetailsView = inflater.inflate(R.layout.category_details_fragment, container, false)
+        setHasOptionsMenu(true)
+
+        bookLiveData.categoryData.observe(viewLifecycleOwner, { category ->
+            with(category) {
+                (activity as AppCompatActivity).title = category_name
+                category_details_item.layoutManager =
+                    LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                category_details_item.adapter = CategoryDetailsAdapter(books as ArrayList<Books>)
+            }
+        })
+
+        return categoryDetailsView
+    }
+
 }
--- app/src/main/java/com/example/e_books/fragments/CategoryFragment.kt
+++ app/src/main/java/com/example/e_books/fragments/CategoryFragment.kt
@@ -1,13 +1,19 @@
 package com.example.e_books.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.navGraphViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.e_books.R
 import com.example.e_books.adapters.CategoryAdapter
 import com.example.e_books.extentions.castCategoryData
 import com.example.e_books.model.Category
+import com.example.e_books.services.BookLiveData
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -17,13 +23,20 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.category_fragment.*
 
 
-class CategoryFragment : Fragment(R.layout.category_fragment) {
+class CategoryFragment : Fragment(R.layout.category_fragment), CategoryAdapter.OnItemClickListener {
 
     private lateinit var db: FirebaseDatabase
-    private lateinit var model: Category
+    private lateinit var categoryView: View
+    private val bookLiveData: BookLiveData by navGraphViewModels(R.id.books_nav)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        categoryView = inflater.inflate(R.layout.category_fragment, container, false)
+        (activity as AppCompatActivity).title = getString(R.string.app_name)
+        setHasOptionsMenu(true)
 
         db = Firebase.database
 
@@ -33,7 +46,8 @@ class CategoryFragment : Fragment(R.layout.category_fragment) {
                     val memberList = it.value as ArrayList<*>
                     category_item.layoutManager =
                         LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-                    category_item.adapter = CategoryAdapter(castCategoryData(memberList))
+                    category_item.adapter =
+                        CategoryAdapter(castCategoryData(memberList), this@CategoryFragment)
                 }
 
             }
@@ -42,5 +56,12 @@ class CategoryFragment : Fragment(R.layout.category_fragment) {
                 TODO("Not yet implemented")
             }
         })
+        return categoryView
     }
+
+    override fun onSeeMoreClick(category: Category) {
+        bookLiveData.setCategory(category)
+        categoryView.findNavController().navigate(R.id.category_details_fragment)
+    }
+
 }
