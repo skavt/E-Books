cb863bc70a54febc0cc2ed41c7fda32854796039
chilaa <farnnn.ff@gmail.com>
2021-01-25 01:15:02 +0400

    Create User page fragment.

---
+++ app/src/main/java/com/example/e_books/fragments/ProfileFragment.kt
@@ -0,0 +1,18 @@
+package com.example.e_books.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.e_books.R
+
+class ProfileFragment: Fragment(R.layout.profile_fragment) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
